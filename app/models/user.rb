2dea97bd7ddc950e8e34c4812c9f55d9c2616db2
Hoang Mirs <hoang.mirs@gmail.com>
2020-07-17 22:14:12 +0700

    Remove redundant tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable, :rememberable, :validatable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable
 
-  has_many :access_grants,
-           class_name: 'Doorkeeper::AccessGrant',
-           foreign_key: :resource_owner_id,
-           inverse_of: :user,
-           dependent: :destroy
-
   has_many :access_tokens,
            class_name: 'Doorkeeper::AccessToken',
            foreign_key: :resource_owner_id,
