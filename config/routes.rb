01ff7c7970723cfd6bf57dfffbe3dfef06a363db
Hoang Mirs <hoang.mirs@gmail.com>
2020-07-22 17:22:37 +0700

    Change route name and rename spec

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       devise_scope :user do
-        post 'forgot_password', to: 'passwords#create'
+        resources :passwords, only: :create
       end
     end
   end
