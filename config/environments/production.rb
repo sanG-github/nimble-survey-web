57ecedc270286fa8a3e175d4195492887403d48b
Hoang Mirs <hoang.mirs@gmail.com>
2020-07-31 16:43:33 +0700

    Add mailgun ENV to ARGs in dockerfile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,11 +72,11 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    port: ENV['MAILGUN_SMTP_PORT'],
-    address: ENV['MAILGUN_SMTP_SERVER'],
-    user_name: ENV['MAILGUN_SMTP_LOGIN'],
-    password: ENV['MAILGUN_SMTP_PASSWORD'],
-    domain: ENV['APP_DOMAIN'],
+    port: ENV.fetch('MAILGUN_SMTP_PORT'),
+    address: ENV.fetch('MAILGUN_SMTP_SERVER'),
+    user_name: ENV.fetch('MAILGUN_SMTP_LOGIN'),
+    password: ENV.fetch('MAILGUN_SMTP_PASSWORD'),
+    domain: ENV.fetch('APP_DOMAIN'),
     authentication: :plain,
   }
 
