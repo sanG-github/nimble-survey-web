3fde889c5cd2e53fe9e4b03b921d4af163468666
Hoang Mirs <hoang.mirs@gmail.com>
2020-08-06 23:32:15 +0700

    Add some UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,26 +8,41 @@ module.exports = {
         sans: ['Neuzeit S LT Std', ...defaultTheme.fontFamily.sans],
         mono: ['Neuzeit S LT Std', ...defaultTheme.fontFamily.mono]
       },
-      colors: {
-        white: {
-          lightest: 'var(--color-white-lightest)',
-          lighter: 'var(--color-white-lighter)',
-          light: 'var(--color-white-light)',
-          default: 'var(--color-white)'
-        }
-      },
-      textColors: {
-        white: {
-          lightest: 'var(--color-white-lightest)',
-          lighter: 'var(--color-white-lighter)',
-          light: 'var(--color-white-light)',
-          default: 'var(--color-white)'
-        }
+      fontSize: {
+        xs: ['13px', '18px'],
+        sm: ['15px', '20px'],
+        base: ['17px', '22px']
       },
       letterSpacing: {
         normal: '-0.41px',
         wide: '-0.24px',
         wider: '-0.08px'
+      },
+      height: {
+        14: '56px'
+      },
+      colors: {
+        white: {
+          default: 'var(--color-white)',
+          180: 'var(--color-white-180)',
+          200: 'var(--color-white-200)',
+          300: 'var(--color-white-300)',
+          500: 'var(--color-white-500)',
+          600: 'var(--color-white-600)'
+        }
+      },
+      textColors: {
+        white: {
+          default: 'var(--color-white)',
+          180: 'var(--color-white-180)',
+          200: 'var(--color-white-200)',
+          300: 'var(--color-white-300)',
+          500: 'var(--color-white-500)',
+          600: 'var(--color-white-600)'
+        }
+      },
+      borderRadius: {
+        lg: '10px'
       }
     }
   },
