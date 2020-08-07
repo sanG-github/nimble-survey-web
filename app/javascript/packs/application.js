a3ba72400888e77d1f5bd848dcb9c9c362512087
Hoang Mirs <hoang.mirs@gmail.com>
2020-08-08 00:32:45 +0700

    Move stylesheets folder into javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ require('@rails/activestorage').start();
 require('channels');
 
 import 'translations/translations';
-import '../../assets/stylesheets/application.scss';
+import '../stylesheets/application.scss';
 
 import 'initializers/';
 import 'screens/';
