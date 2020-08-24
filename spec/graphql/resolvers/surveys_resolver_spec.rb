1406dcc307c2dee5b01eae3b6c5ab82c83611274
Hoang Mirs <hoang.mirs@gmail.com>
2020-08-24 17:45:38 +0700

    Add specs for pagination

--- spec/graphql/resolvers/surveys_resolver_spec.rb
+++ spec/graphql/resolvers/surveys_resolver_spec.rb
@@ -3,25 +3,121 @@
 require 'rails_helper'
 
 RSpec.describe Resolvers::SurveyResolver do
-  it 'returns all surveys' do
-    query_string = <<~GRAPHQL
-      query Surveys {
-        surveys {
-            id
-            title
-            description
+  context 'given no pagination params' do
+    it 'returns all surveys' do
+      query_string = <<~GRAPHQL
+        query Surveys {
+          surveys {
+            total_count
+            edges {
+              node {
+                id
+                title
+                description
+                questions {
+                  id
+                }
+              }
+            }
+          }
         }
-      }
-    GRAPHQL
+      GRAPHQL
 
-    result = NimbleSurveyWebSchema.execute(query_string, variables: {})
+      result = NimbleSurveyWebSchema.execute(query_string, variables: {})
 
-    surveys = result['data']['surveys']
-    expect(surveys).to be_an(Array)
+      surveys = result['data']['surveys']
+      expect(surveys['total_count']).to eq(20)
+      expect(surveys['edges']).to be_an(Array)
+      expect(surveys['edges'].size).to eq(20)
+    end
 
-    survey = surveys.first
-    expect(survey['id']).to be_present
-    expect(survey['title']).to be_present
-    expect(survey['description']).to be_present
+    it 'returns the requested fields' do
+      query_string = <<~GRAPHQL
+        query Surveys {
+          surveys {
+            total_count
+            edges {
+              node {
+                id
+                title
+                description
+                questions {
+                  id
+                }
+              }
+            }
+          }
+        }
+      GRAPHQL
+
+      result = NimbleSurveyWebSchema.execute(query_string, variables: {})
+
+      surveys = result['data']['surveys']['edges']
+      first_survey = surveys.first['node']
+
+      expect(first_survey['id']).to be_present
+      expect(first_survey['title']).to be_present
+      expect(first_survey['description']).to be_present
+      expect(first_survey['questions']).to be_an(Array)
+    end
+  end
+
+  context 'given pagination params' do
+    it 'returns the correct requested surveys' do
+      query_string = <<~GRAPHQL
+        query Surveys {
+          surveys(first: 2) {
+            total_count
+            edges {
+              node {
+                id
+                title
+                description
+                questions {
+                  id
+                }
+              }
+            }
+          }
+        }
+      GRAPHQL
+
+      result = NimbleSurveyWebSchema.execute(query_string, variables: {})
+
+      surveys = result['data']['surveys']
+      expect(surveys['total_count']).to eq(20)
+      expect(surveys['edges']).to be_an(Array)
+      expect(surveys['edges'].size).to eq(2)
+    end
+
+    it 'returns the requested fields' do
+      query_string = <<~GRAPHQL
+        query Surveys {
+          surveys {
+            total_count
+            edges {
+              node {
+                id
+                title
+                description
+                questions {
+                  id
+                }
+              }
+            }
+          }
+        }
+      GRAPHQL
+
+      result = NimbleSurveyWebSchema.execute(query_string, variables: {})
+
+      surveys = result['data']['surveys']['edges']
+      first_survey = surveys.first['node']
+
+      expect(first_survey['id']).to be_present
+      expect(first_survey['title']).to be_present
+      expect(first_survey['description']).to be_present
+      expect(first_survey['questions']).to be_an(Array)
+    end
   end
 end
