2cec359238c19264c6e21e13c73c156ba2b40866
Hoang Mirs <hoang.mirs@gmail.com>
2020-09-02 12:51:31 +0700

    Enable core-js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ require('@rails/ujs').start();
 require('@rails/activestorage').start();
 require('channels');
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
 import 'translations/translations';
 
 import 'stylesheets/application.scss';
