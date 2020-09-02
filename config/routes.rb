21ab979617ef38c262e1ebbc5254b990818d1088
Hoang Mirs <hoang.mirs@gmail.com>
2020-09-02 10:07:48 +0700

    Add some notes to routes file

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,16 @@ Rails.application.routes.draw do
   end
 
   use_doorkeeper do
+    # Only use applications_controller to manage OAuth2 applications
+    # and mount it outside api/v1 as it has the views
+
     skip_controllers :tokens, :authorizations, :token_info, :authorized_applications
   end
 
   scope :api do
     scope :v1 do
       use_doorkeeper do
+        # Only need the tokens_controller for API V1
         controllers tokens: 'api/v1/tokens'
 
         skip_controllers :applications, :authorizations, :token_info, :authorized_applications
