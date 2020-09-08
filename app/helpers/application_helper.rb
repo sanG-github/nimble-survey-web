e0f8a4df87d2c232658a1b3eeb9c4bea3a54a717
Hoang Mirs <hoang.mirs@gmail.com>
2020-09-08 14:09:05 +0700

    Refactor translations

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,9 +2,9 @@
 
 module ApplicationHelper
   def page_title
-    [page_title_prefix, t('page_title.general')].join(' | ')
+    [page_title_prefix, t('shared.page_title')].join(' | ')
   rescue I18n::MissingTranslationData
-    t('page_title.general')
+    t('shared.page_title')
   end
 
   private
@@ -12,6 +12,6 @@ module ApplicationHelper
   def page_title_prefix
     return content_for :page_title if content_for?(:page_title)
 
-    t("page_title.#{controller_name}.#{action_name}", raise: true)
+    t("#{controller_name}.#{action_name}.page_title", raise: true)
   end
 end
