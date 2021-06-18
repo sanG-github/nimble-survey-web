35553fa002e7521c81d1f5d231c5ff80a1de630e
Hoang Mirs <hoang.mirs@gmail.com>
2021-06-18 12:52:08 +0700

    Modify serializer and graphql resolver to return name

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,6 +4,7 @@ module Types
   class UserType < Types::Base::Object
     field :id, ID, null: false
     field :email, String, null: false
+    field :name, String, null: false
     field :avatar_url, String, null: false
   end
 end
