9517d950d7653c02301b4ac36fc1b578d458d17b
Hoang Mirs <hoang.mirs@gmail.com>
2021-06-28 16:02:11 +0700

    Fix danger comment

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
 class UserDecorator < SimpleDelegator
-  AVATAR_URL_FORMAT = 'https://secure.gravatar.com/avatar/%{gravatar_id}'
-
   def avatar_url
-    gravatar_id = Digest::MD5::hexdigest(email)
-    format(AVATAR_URL_FORMAT, gravatar_id: gravatar_id)
+    gravatar_id = Digest::MD5.hexdigest(email)
+    "https://secure.gravatar.com/avatar/#{gravatar_id}"
   end
 end
