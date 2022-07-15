97f8614f119ffef04cb60076284b6b427e8222dd
Hoang Mirs <hoang.mirs@gmail.com>
2022-07-15 13:19:30 +0700

    [#70] Survey list API should not return the question id list relationship

--- app/serializers/survey_serializer.rb
+++ app/serializers/survey_serializer.rb
@@ -1,17 +1,5 @@
 # frozen_string_literal: true
 
-class SurveySerializer < ApplicationSerializer
-  attributes :title,
-             :description,
-             :thank_email_above_threshold,
-             :thank_email_below_threshold,
-             :is_active,
-             :cover_image_url,
-             :created_at,
-             :active_at,
-             :inactive_at
-
-  attribute :survey_type, &:type
-
+class SurveySerializer < SurveyListSerializer
   has_many :questions
 end
