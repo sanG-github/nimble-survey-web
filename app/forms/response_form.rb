1985c8b6ffb8e2e2271c59b9ec9622831e945e46
Su Ho <su.v@nimblehq.co>
2023-01-13 16:55:06 +0700

    [#80] Improve the validating logic

--- app/forms/response_form.rb
+++ app/forms/response_form.rb
@@ -26,7 +26,7 @@ class ResponseForm < ApplicationForm
     survey_questions_ids = survey.questions.map(&:id)
     questions_ids = questions.pluck(:id)
 
-    return if (survey_questions_ids & questions_ids).to_set == questions_ids.to_set
+    return if (questions_ids & survey_questions_ids) == questions_ids
 
     errors.add(:questions, :invalid)
   end
